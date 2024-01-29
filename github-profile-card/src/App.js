541f7a6951a00bd42cc7abc9b86a5ef453f45b8b
GeorgiAleksandrovv <joro111work@gmail.com>
2024-01-30 01:08:29 +0200

    center the background

--- github-profile-card/src/App.js
+++ github-profile-card/src/App.js
@@ -46,28 +46,30 @@ function App() {
   return (
     <div className="App">
       {userData && (
-        <div className="profile-card">
-          <img
-            className="profile-pic"
-            src={userData.avatar_url}
-            alt="Profile Picture"
-          />
-          <h1 className="username">{userData.login}</h1>
-          <h2 className="bio">{userData.bio}</h2>
-          <h2 className="normal-text">Number of Repositories: {repoCount}</h2>
-          <h2 className="normal-text">
-            Languages Used: {languages.join(", ")}
-          </h2>
+        <div className="background">
+          <div className="profile-card">
+            <img
+              className="profile-pic"
+              src={userData.avatar_url}
+              alt="Profile Picture"
+            />
+            <h1 className="username">{userData.login}</h1>
+            <h2 className="bio">{userData.bio}</h2>
+            <h2 className="normal-text">Number of Repositories: {repoCount}</h2>
+            <h2 className="normal-text">
+              Languages Used: {languages.join(", ")}
+            </h2>
 
-          <div className="social-container">
-            <div className="followers">
-              <h1 className="bold-text">{followers}</h1>
-              <h2 className="smaller-text">Followers</h2>
-            </div>
+            <div className="social-container">
+              <div className="followers">
+                <h1 className="bold-text">{followers}</h1>
+                <h2 className="smaller-text">Followers</h2>
+              </div>
 
-            <div className="followers">
-              <h1 className="bold-text">{following}</h1>
-              <h2 className="smaller-text">Following</h2>
+              <div className="followers">
+                <h1 className="bold-text">{following}</h1>
+                <h2 className="smaller-text">Following</h2>
+              </div>
             </div>
           </div>
         </div>
